7c287dc9d7d36adcaaf8d21ca64477f81a28b4a9
merrittzhang <zhangmerritt@gmail.com>
2025-03-30 14:30:31 -0400

    add calls to user.h

--- solution/user.h
+++ solution/user.h
@@ -23,6 +23,10 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int clone(void *stack);
+int join(void);
+int lock(int *l);
+int unlock(int *l);
 
 // ulib.c
 int stat(const char*, struct stat*);
