bbf926aff15e6a3ac96ffd5801f4c0f78d2627cf
merrittzhang <zhangmerritt@gmail.com>
2025-03-30 21:32:45 -0400

    debug

--- solution/main.c
+++ solution/main.c
@@ -17,7 +17,6 @@ extern char end[]; // first address after kernel loaded from ELF file
 int
 main(void)
 {
-  lockinit(); 
   kinit1(end, P2V(4*1024*1024)); // phys page allocator
   kvmalloc();      // kernel page table
   mpinit();        // detect other processors
